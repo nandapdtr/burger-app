8e18034de7a906f7cea6fd97878fbfd8d64ca22c
x014378 <nanda.rabba@klm.com>
2020-05-12 22:41:08 +0200

    added simple unit test

---
+++ src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+import NavigationItems from './NavigationItems';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+configure({ adapter: new Adapter() });
+
+describe('NavigationItems', () => {
+    let wrapper;
+
+    beforeEach(() => {
+        wrapper = shallow(<NavigationItems />);
+    });
+
+    it('should render 2 NavigationItem elements', () => {
+        expect(wrapper.find(NavigationItem)).toHaveLength(2);
+    });
+
+    it('should render 3 NavigationItem elements', () => {
+        wrapper.setProps({ isAuth: true });
+        expect(wrapper.find(NavigationItem)).toHaveLength(3);
+    });
+});
